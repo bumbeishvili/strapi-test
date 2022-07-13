a27230ad432312044f3bd4f4eedfa69f3c9a6bfd
bumbeishvili <me@davidb.dev>
2022-07-13 17:02:03 +0400

    front end files

---
+++ frontend/src/pages/Homepage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReviewDetails from './ReviewDetails';
+
+export default function Homepage() {
+    return (<div>
+        <h1>Homepage</h1>
+    </div>)
+}
---
+++ frontend/src/pages/ReviewDetails.js
@@ -0,0 +1,10 @@
+import { React } from 'react';
+import Category from './Category';
+
+export default function ReviewDetails() {
+    return (
+        <div>
+            <h1>ReviewDetails</h1>
+        </div>
+    );
+}
